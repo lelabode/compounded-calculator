48d67e9b3134cb0ef2ab2ba477e315f76ef8b5aa
aurelabode <lelabode@hotmail.com>
2025-11-11 17:13:01 +0100

    Random numbers

---
+++ my_c_projet/random.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+# include <stdlib.h>
+# include <time.h>
+
+int main(){
+ srand(time(NULL));
+ int min = 1;
+ int max  =100;
+ int randomNum1 = (rand()%(max -min +1)+ min);
+ int randomNum2 = (rand() % (max -min +1) +min);
+ int randomNum3 = ( rand() % (max - min +1) +min);
+
+ printf(" %d %d %d ", randomNum1, randomNum2, randomNum3);
+
+
+
+
+
+
+  return 0;
+}
